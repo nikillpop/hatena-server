95149f1675fcd5e36d7cf2cada20392b97e4ec3f
Dylan Myers <daylosignup@gmail.com>
2015-06-05 19:28:32 +0100

    v0.7-alpha - Logging, LESS, Region redirection, minor fixes
    In this version:
    * Logging with Bunyan (uses Redbird, no need for additional package)
    * Starting out with LESS for CSS.
    * Redirecting v2-eu,us,etc to v2-xx
    * Minor fixes including using UTF-16 where UTF-8 is illegal, removing
    v2-eu because of the previous entry, etc
    **** NEW REQUIREMENT: npm install mkdirp ****
    Coming Soon/To Do:
    * Ugo + UGOXML implementation
    * CSS evaluation (see unused elements)
    * SCSS and finishing LESS

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # hatena-server for NodeJS
-
+### version 0.7-alpha
 hatena-server is a replacement server that acts just how Flipnote client likes it.
 
 ## Requires
@@ -7,20 +7,30 @@ hatena-server is a replacement server that acts just how Flipnote client likes i
  * [NodeJS](https://nodejs.org/)
  * expressJS - ``` npm install express ```
  * redbird - ``` npm install redbird ```
+ * mkdirp - ``` npm install mkdirp ```
 
 ## Installing
 
     $ git clone git://github.com/dylmye/hatena-server.git
     $ cd hatena-server
 
-    et voila :)
+There are some parts you need to configure yourself:  
+
+ * ```@str hosta``` - being the server you're hosting hatena-server (this software) on. Default: your external ip
+ * ```@str porta``` - being the port the server is being hosted on. NEVER USE 8080, THIS IS WHAT THE PROXY USES Default: "3020"
+ * ```@bol doLog``` - enable / Disable Logging. Default: true
+ * ```@str workDir``` - where the files are hosted. Bear in mind the client requests ```http://flipnote.hatena.com/ds/v2-xx/file``` (xx being a region code). Default: "./public/"
+ * ```@str logDir``` - where logs are stored. Created by bunyan. Default: "./logs/"  
+
+The proxy is hosted on localhost, and clients can connect to your external IP, on port 8080.
 
 ## Usage
 
     $ node server.js
 
+Software is in Alpha stage. No guarantee is made of the accuracy, completness or functionality presented within this document or the software bundled with it.
 ## Copyright
 
-Original (c) Hatena Co., Ltd
-Python version (c) pbsds under GNU AFFERO 3
-NodeJS port (c) dylmye under ISC
+Original (c) Hatena Co., Ltd  
+Python version (c) pbsds under GNU AFFERO 3  
+NodeJS port (c) dylmye under ISC  
--- server.js
+++ server.js
@@ -1,55 +1,106 @@
 // hatena-server-nodejs
 // original - pbsds (sha-2 23e198ccf068539a2fc374bc2f8571a36d057d29)
-// ported   - dylmye (v0.5-alpha)
+// ported   - dylmye (v0.7-alpha)
 
-// @requires:
-//    ¬ nodeJS (server provider)
-//    ¬ expressJS (server)
-//    ¬ redbird (proxy)
+/* @requires:
+    ¬ nodeJS (server provider)
+    ¬ expressJS (server)
+    ¬ redbird (proxy)
+    ¬ mkdirp (to make the log directory)
+*/
 
-// changelog
-//    ¬ v0.1-alpha - basic server design
-//    ¬ v0.5-alpha - proxy added
-// NO UGO WORKING == NOT REALLY WORKING
+/* changelog
+    ¬ v0.1-alpha - basic server design
+    ¬ v0.5-alpha - proxy added
+    ¬ v0.7-alpha - added logging (uses built-in bunyan) + rewriting v2-eu etc to -xx
+    ¬ v0.8-alpha - FUTURE - planning to add UGO functionality
+*/
 
-// DEPENDENCIES
-var express = require('express');
-    redbird = require('redbird')({port: 8080});
+console.log("hatena-server-nodejs # dylmye # pbsds # v0.7-alpha");
+function getLogDate(isLog) {
+    var now     = new Date();
+    var year    = now.getFullYear();
+    var month   = now.getMonth() + 1;
+    var day     = now.getDate();
+
+    if(month.toString().length == 1) {
+        var month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+        var day = '0'+day;
+    }
+    if (isLog) {
+        var logDate = year + '/' + month + '/' + day + '.log'; // for redbird
+    }
+    else {
+        var logDate = year + '/' + month; // for mkdir
+    }
+    return logDate;
+}
 
 // EDIT THESE
-var port = 3000; // change to whatever
-var silrun = 0; // if 1, no logging happens
-var workDir = "./hatenadir/"; // where files are served
+var hosta = "YOURSERVERHERE"; // your server location
+var porta = "3020"; // port for server
+var doLog = new Boolean(true); // if false, no logging happens
+var workDir = "./public/"; // where files are served
+var logDir = "./logs/"; // where logs are stored
+
+// DEPENDENCIES
+console.log("UPDATE: Importing Modules");
+var express = require('express');
+var mkdirp = require('mkdirp');
+mkdirp(logDir + getLogDate(false), function (err) { // create logging dir
+    if (!err) { // hooray! directory is set up
+        console.log("UPDATE: Directory created/already existent")
+    }
+    else if (err) { // some error: tell user
+        console.log("ERROR: Couldn't create directory: " + err)
+    }
+});
+if (doLog) { // if logging is enabled
+    var redbird = new require('redbird')({
+        port: 8080,
+        host: "http://localhost",
+        bunyan: {
+            name: "hatenaserver",
+            streams: [{
+                path: "./logs/" + getLogDate(true), // log file
+            }]
+        }
+    });
+}
+else { // if logging is disabled
+    var redbird = new require('redbird')({ port: 8080 })
+}
+console.log("UPDATE: Modules imported");
+
 
 // REST OF THE STUFF
 
 // Host server
+console.log("UPDATE: Attempting execution of Express");
 var app = express(); // this is just the beginning
-console.log("Express set up! ... (1/5)");
+console.log("UPDATE: Express App executed");
 
 app.get(app.use(express.static(workDir)), function (req, res) { // open up hatenadir directory
-  console.log("Directory accessed! ... (2/5)");
+  console.log("UPDATE: Files are accessible");
 });
 
-var server = app.listen(port, function () { // open server up
-  var host = server.address().address;
-  var port = port;
-  console.log("Server set up! Connect to " + host + ":" + port + ". (3/5)");
+var server = app.listen(porta, function () { // open server up
+  var host = hosta;
+  var port = porta;
+  console.log("UPDATE: Server up");
 });
 
 // Proxy
+console.log("UPDATE: Attempting Proxy Config");
+redbird.register("flipnote.hatena.com/ds/v2-eu/", hosta + ":" + porta + "/ds/v2-xx"); // europe users to worldwide
+redbird.register("flipnote.hatena.com/ds/v2-us/", hosta + ":" + porta + "/ds/v2-xx"); // american users to worldwide
+redbird.register("flipnote.hatena.com/ds/v2-xx/", hosta + ":" + porta + "/ds/v2-xx"); // other users to worldwide
 
-redbird.register("flipnote.hatena.com", "localhost:" + port);
+console.log("UPDATE: Proxy Functional");
 
-console.log("Proxy set up, nearly there! (4/5)");
-
-// Logging
-
-  // create classes of each type of error/request
-  // 403
-  // 200
-console.log("WARNING: Logging not set up");
-console.log("All ready to go! (5/5)");
+console.log("hatena-server is up!");
 
 // todo:
-//    Logging, "Handles", Plaintext database implementation, ugoxml implementation, HATENA-TOOLS handling, connection-test
+//    Plaintext database implementation, ugoxml implementation, HATENA-TOOLS handling
